ffec0aaaebc13639aa854860e2f3152eaf97dbea
anna gileva <tyuiu.gilevaap@outlook.com>
2026-09-17 18:30:52 +0500

    t

---
+++ Tyuiu.GilevaAP.Sprint1.Task0.V1.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.GilevaAP.Sprint1.Task0.V1.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.GilevaAP.Sprint1.Task0.V1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.GilevaAP.Sprint1.Task0.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
