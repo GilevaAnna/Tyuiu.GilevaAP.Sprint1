2e2d7fabc7a8a8b3df27ad0b81cd448d76a578b8
anna gileva <tyuiu.gilevaap@outlook.com>
2026-09-21 13:09:06 +0500

    Task2

---
+++ Tyuiu.GilevaAP.Sprint1.Task2.V11.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.GilevaAP.Sprint1.Task2.V11.Lib;
+namespace Tyuiu.GilevaAP.Sprint1.Task2.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void Example()
+        {
+            DataService ds = new DataService();
+            int value = 3;
+            int valueTwo = 45;
+            var res = ds.ConvertHoursMinutesToSeconds(value, valueTwo);
+            Assert.AreEqual(13500, res);
+        }
+    }
+}
---
+++ Tyuiu.GilevaAP.Sprint1.Task2.V11/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.GilevaAP.Sprint1.Task2.V11.Lib;
+namespace Tyuiu.GilevaAP.Sprint1.Task2.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Гилёва А. П. | СМАРТб-26-1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Создания итогового решения по спринту                             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #11                                                             *");
+            Console.WriteLine("* Выполнила: Гилёва Анна Петровна | СМАРТб-26-1                           *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет сколько секунд в часах и минутах  *");
+            Console.WriteLine("* и печатает результат на экране.                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            double value, valueTwo;
+            Console.WriteLine("* Введите значение value:");
+            value = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("* Введите значение valueTwo:");
+            valueTwo = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(ds.ConvertHoursMinutesToSeconds((int)value, (int)valueTwo));
+            Console.ReadLine();
+        }
+    }
+}
